796321679542041937585c1afbf7c51c496f0339
AniketSupekar <aniketsupekar2003@gmail.com>
2023-12-20 22:05:19 +0530

    Added media section, fixed bugs in about section

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+import About from './components/homepage/About/About'
+import Media from './components/homepage/Media/media'
+
+function App() {
+  
+  return (
+    <div>
+      <About></About>
+      <Media></Media>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/homepage/Footer/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+        <div className='flex'>
+            <div>
+                {/* <img src='https://res.cloudinary.com/da5mmxnn3/image/upload/v1675598960/vp-webp/edc_logo-removebg-preview_mhknvu.webp'/> */}
+                <h1 className='text-md'>ENTREPRENEURSHIP DEVELOPMENT CELL, VIIT</h1>
+                <p>Presents</p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
